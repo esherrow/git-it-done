f0a44269e713fe25e188e34cd53fab15c10fa3b9
Yoga Cats <esherrow77@gmail.com>
2021-05-12 21:34:24 -0400

    featured repo search, language search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
@@ -77,5 +78,29 @@ var displayRepos = function(repos, searchTerm){
     }
 };
 
+var getFeaturedRepos = function(language){
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub user Not Found')
+        }
+    });
+
+};
+
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+    if(language){
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";}
+
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formSubmitHandler);
 //getUserRepos();
